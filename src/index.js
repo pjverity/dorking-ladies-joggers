c61cffbbc77af9c6467db1ecede6ae22c65cba4b
Paul Verity <paul.verity@v-home.co.uk>
2018-04-30 22:25:43 +0100

    dorking-ladies-joggers-#10: Allow admins to amend schedules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { h, render, Component } from 'preact';
+import { h, render, Component } from 'preact'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'popper.js'
 
 import App from './components/App.jsx';
 
