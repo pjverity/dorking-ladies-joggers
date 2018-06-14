baf664d84312e7f2f8c783e6f7351a6a74fffab7
Paul Verity <paul.verity@v-home.co.uk>
2018-06-14 09:15:05 +0100

    Tidy-up: Removed domain from API URL and added dev web proxy config

--- src/site-constants.js
+++ src/site-constants.js
@@ -1,10 +1,9 @@
-
 const INDEX_TITLE = 'Dorking Ladies Joggers';
 const INDEX_META_DESCRIPTION = 'Home of Ladies Run Club in Dorking, Surrey';
 
 const FACEBOOK_URL = '/dorkingladiesjoggers';
 const SITE_EMAIL_ADDRESS = 'admin@dorkingladiesjoggers.co.uk';
-const SITE_API_URL = 'http://www.dorkingladiesjoggers.co.uk/enquiries';
+const SITE_API_URL = '/enquiries';
 
 module.exports = {
 	INDEX_TITLE,
